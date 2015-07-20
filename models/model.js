2645a8f5c487ab5eb358c7a3c3ca192956a295c8
Alex Chan <alexchan@Alexs-MacBook-Pro-3.local>
2015-07-20 13:38:09 -0700

    updated server

---
+++ models/model.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var RecipeSchema = new Schema({
+    image_url: String,
+    title: String,
+    source_url: String,
+});
+
+var Recipe = mongoose.model('Recipe', RecipeSchema);
+
+module.exports = Recipe;
